6fc37dd9a4b4435d62820e2042259202e6f6935d
harluz <harluz06@gmail.com>
2023-02-09 00:04:19 +0900

    [add] 予約の新規登録機能を追加

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -4,10 +4,18 @@ class SchedulesController < ApplicationController
   end
 
   def new
+    @schedule = Schedule.new
   end
 
   def create
-    
+    @schedule = Schedule.new(params.require(:schedule).permit(:title, :memo, :start_date, :end_date, :all_day))
+    if @schedule.save
+      flash[:notice] = "予約が完了しました"
+      
+      redirect_to schedules_path
+    else
+      render "new"
+    end
   end
   
   def show
