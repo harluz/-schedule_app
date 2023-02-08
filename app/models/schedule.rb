5eb9b355ebe5a7fe9f4fab3c1a993d6f308f6fb8
harluz <harluz06@gmail.com>
2023-02-08 20:32:32 +0900

    [fix] バリデーションの「validates :start_end_check」を「validate :start_end_check」に修正したもの

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,13 +1,13 @@
 class Schedule < ApplicationRecord
-  validates :title, presence :true, length: {maximum: 20}
+  validates :title, presence: true, length: {maximum: 20}
   validates :memo, length: {maximum: 500 }
-  validates :start_date, presence :true
-  validates :end_date, presence :true
-  validates :start_end_check
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validate :start_end_check
 
   def start_end_check
     if self.end_date < self.start_date
-      errors.add(:end_date, "は開始日より前の日付は登録できません")
+      errors.add(:end_date, message: "は開始日より前の日付は登録できません")
     end
   end
 end
