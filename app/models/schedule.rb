b8bfebdd0d5590858e95f13f9b60226b83192103
harluz <harluz06@gmail.com>
2023-02-08 19:18:14 +0900

    [add] Scheduelモデルにバリデーションを追加

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,2 +1,13 @@
 class Schedule < ApplicationRecord
+  validates :title, presence :true, length: {maximum: 20}
+  validates :memo, length: {maximum: 500 }
+  validates :start_date, presence :true
+  validates :end_date, presence :true
+  validates :start_end_check
+
+  def start_end_check
+    if self.end_date < self.start_date
+      errors.add(:end_date, "は開始日より前の日付は登録できません")
+    end
+  end
 end
